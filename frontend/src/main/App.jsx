98e1eafb4f2672f62c32d8d26f1b9f321a110ff7
Mateus <mateusfnogueira@outlook.com>
2020-05-27 14:06:52 -0300

    Create App.jsx

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,7 @@
+import './App.css'
+import React from 'react'
+
+import Logo from '../components/template/Logo'
+import Main from '../components/template/Main'
+import Nav from '../components/template/Nav'
+import Footer from '../components/template/Footer'
