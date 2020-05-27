3dcdf1b8c832fe898ddbd249932052321d309118
Mateus <mateusfnogueira@outlook.com>
2020-05-27 17:01:14 -0300

    Update Main.jsx

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -6,7 +6,9 @@ export default props =>
     <React.Fragment>
         <Header {...props}/>
             <main className="content container-fluid">
-                Conteúdo
+                <div className="p-3 mt-3">
+                    {props.children}
+                </div>
             </main>
         
     </React.Fragment>
