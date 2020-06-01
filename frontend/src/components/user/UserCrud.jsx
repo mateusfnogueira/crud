eae5065a885c912e1dff27c2d23dc7516c64c07b
Mateus <mateusfnogueira@outlook.com>
2020-06-01 10:20:56 -0300

    Update UserCrud.jsx

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -108,6 +108,44 @@ export default class UserCrud extends Componet {
         })
     }
 
+    renderTable() {
+        return(
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        )
+    }
+
+    renderRows() {
+        return this.state.list.map(user => {
+            return (
+                < tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button className="btn btn-warning">
+                            onClick={()=> this.load(user)}
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger ml-2">
+                            onClick={() => this.remove(user)}
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
     render() {
         console.log(this.state.list)
 
