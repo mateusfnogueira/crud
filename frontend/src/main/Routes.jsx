ba8258687b1b05c8190e32cb8e9eef3c59b845a1
Mateus <mateusfnogueira@outlook.com>
2020-06-09 13:00:26 -0300

    Update Routes.jsx

--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -4,7 +4,7 @@ import {Switch, Route, Redirect} from 'react-router'
 import Home from '../components/home/Home'
 import UserCrud from '../components/user/UserCrud'
 
-export default prop =>
+export default props =>
     <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/users' component={UserCrud}/>
